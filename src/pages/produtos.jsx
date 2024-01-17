11cfee8d32f0f5199963af43af65d29ddcd120a3
victor <victorhugomotabueno@gmail.com>
2024-01-17 11:29:09 -0300

    mini exercicio metatag e title

--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -1,6 +1,13 @@
+import Head from "next/head";
+
 export default function Produtos() {
   return (
     <>
+      <Head>
+        <title>Produtos</title>
+        <meta name="description" content="Produtos do Petshop" />
+      </Head>
+
       <section>
         <h2>Conheça nossos produtos</h2>
       </section>
--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -1,6 +1,13 @@
+import Head from "next/head";
+
 export default function Sobre() {
   return (
     <>
+      <Head>
+        <title>Página sobre o Petshop</title>
+        <meta name="description" content="Sobre o Petshop" />
+      </Head>
+
       <section>
         <h2>Sobre nosso Petshop</h2>
       </section>
