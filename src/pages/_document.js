057d44da65f8cdd182940dbccda8bc5aaf5526d7
victor <victorhugomotabueno@gmail.com>
2024-01-17 09:57:05 -0300

    Favicon, metatag author

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link rel='shortcut icon' href='/images/favicon.png' type='image/png' sizes='256x256' />
+        <meta name='author' content='Victor Bueno' />
+      </Head>
       <body>
         <Main />
         <NextScript />
