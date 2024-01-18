bd8ebf86f1e507465e86f28db063702d284a528f
victor <victorhugomotabueno@gmail.com>
2024-01-18 08:45:50 -0300

    Adicionando Link ao H1 e  usando o Image

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,9 +1,16 @@
 import Menu from "./Menu";
+import Link from "next/link"; 
+import Image from "next/image"; 
 
 export default function Layout( {children} ) {
     return <>
         <header>
-            <h1>Petshop</h1>
+            <h1>
+                <Image src='/images/logo.png' width={48} height={48}
+                alt='Patinha dentro de um coração' />
+
+                <Link href='/'>Petshop</Link>
+            </h1>
             <Menu />
         </header>
     </>
