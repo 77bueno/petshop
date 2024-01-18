b64bf352507bcd51ccfcd21bd6fe7ec92dea7107
victor <victorhugomotabueno@gmail.com>
2024-01-18 09:08:59 -0300

    Instalação do styled components e o css do header

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,17 +1,36 @@
 import Menu from "./Menu";
 import Link from "next/link"; 
 import Image from "next/image"; 
+import styled from "styled-components";
 
 export default function Layout( {children} ) {
     return <>
-        <header>
+        <StyledHeader>
             <h1>
-                <Image src='/images/logo.png' width={48} height={48}
-                alt='Patinha dentro de um coração' />
-
-                <Link href='/'>Petshop</Link>
+                <Link href='/'>
+                    <Image src='/images/logo.png' width={48} height={48}
+                    alt='Patinha dentro de um coração' />
+                    Petshop
+                </Link>
             </h1>
             <Menu />
-        </header>
+        </StyledHeader>
     </>
 }
+
+const StyledHeader = styled.header`
+    background-color: #f7f7f7;
+    box-shadow: var(--sombra-box);
+
+    h1 a {
+        text-decoration: none;
+        color: var(--cor-primaria);
+        background-color: var(--cor-primaria-fundo);
+        padding: 0.5rem 1.2rem;
+        border-radius: var(--borda-arredondada);
+        font-size: 1.5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
