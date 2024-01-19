5aa146d68275e84fd0888303dbecb1f93c607d62
victor <victorhugomotabueno@gmail.com>
2024-01-19 09:10:22 -0300

    Estrutura HTML do formulario

--- src/pages/contato.jsx
+++ src/pages/contato.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styled from "styled-components";
+import Container from "@/components/ui/Container";
 
 export default function Contato() {
   return (
@@ -11,6 +12,36 @@ export default function Contato() {
 
       <StyledContato>
         <h2>Fale conosco</h2>
+
+        <Container>
+          <form action="" method="post">
+            <div>
+              <label htmlFor="nome">Nome: </label>
+              <input type="text" name="nome" id="nome" />
+            </div>
+
+            <div>
+              <label htmlFor="email">Email: </label>
+              <input type="text" name="email" id="email" />
+            </div>
+
+            <div>
+              <label htmlFor="mensagem">Mensagem: </label>
+              <textarea 
+                type="text" 
+                name="mensagem" 
+                id="mensagem" 
+                cols="30" 
+                rows="8" 
+                maxLength={500}>
+              </textarea>
+            </div>
+
+            <div>
+              <button type="submit">Enviar mensagem</button>
+            </div>
+          </form>
+        </Container>
       </StyledContato>
     </>
   )
