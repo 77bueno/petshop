6b5905aefe04660b565e0004296895ecd1105a49
victor <victorhugomotabueno@gmail.com>
2024-01-22 08:20:38 -0300

    .

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,62 +14,14 @@ export default function Home() {
       <StyledHome>
         <h2>Pet Notícias</h2>
 
-        <Styledlistaposts>
-        
-          { arrayPosts.map( (artigo) => {
-            return (
-            <article key={artigo.id}>
-            <Link href="">
-            <h3>{artigo.titulo}</h3>
-            <p>{artigo.subtitulo}</p>
-            </Link>
-            </article>
-            )
-          })}
-          
-        </Styledlistaposts>
-
+        <ListasPosts> qualquerCoisa={arrayPosts} </ListasPosts>
 
       </StyledHome>
     </>
   )
 }
 
-const Styledlistaposts = styled.div`
-  article {
-    background-color: #f7f7f7;
-    padding: 1rem;
-    margin: 0 0 1rem 0;
-    box-shadow: var(--sombra-box);
-    border-radius: var(--borda-arredondada);
-    transition: transform 200ms;
 
-    & a {
-      text-decoration: none;
-      color: black;
-
-    &:hover, 
-    &:focus {
-      color: #0066ff;
-    }
-  }
-}
-
-  article:hover {
-    cursor: pointer;
-    transform: scale(1.05);
-  }
-
-  @media screen and (min-width: 500px) {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    
-    article {
-      width: 49%;
-    }
-  }
-`;
 
 const StyledHome = styled.section`
   h2::before{
