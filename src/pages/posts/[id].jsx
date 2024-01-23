bd61df8c402b3f1c4ecabc3ee29b63550cb87afc
victor <victorhugomotabueno@gmail.com>
2024-01-23 11:04:55 -0300

    Estrutura da página

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,3 +1,26 @@
+import  Head  from "next/head";
+import styled from "styled-components";
+import Container from "@/components/ui/Container";
+
 export default function Post(){
-    return <h2>Página Post!</h2>;
+  return (
+    <>
+      <Head>
+       <title>Título do post... - Petshop</title>
+       <meta name="description" content="Descricao do post" />
+      </Head>
+
+      <StyledPost>
+        <h2>Titulo do Post...</h2>
+        <Container>
+            <h3>Categoria do Post...</h3>
+            <p>Descrição do Post</p>
+        </Container>
+      </StyledPost>
+   </>
+  );
 }
+
+const StyledPost = styled.article`
+     
+`;
