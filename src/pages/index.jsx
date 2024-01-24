02a917a4b1f2bb25fd122333c2e4e8f1f6eb2bf1
victor <victorhugomotabueno@gmail.com>
2024-01-24 09:21:08 -0300

    Retirando comentários

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,6 @@ import ListaPosts from "@/components/ListaPosts";
 import { useState } from "react";
 import serverApi from "./api/server";
 
-  /* Função getStaticProps
-  Utilizado para execução de código server-side (neste caso, fetch na API)
-  com o objeto de gerar props com os dados processados. */
   export async function getStaticProps(){
     console.log("Código de servidor (não aparece no cliente)...");
 
@@ -18,10 +15,6 @@ import serverApi from "./api/server";
         throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`)
       }
 
-        /* Após o processamento (desde que não haja erros), a getStaticProps
-      retorna um objeto com uma propriedade chamada "props", e nesta propriedade
-      colocamos um objeto com as props que queremos usar. No caso, usamos
-      uma prop "posts" (pode ter qualquer nome) e é nela que colocamos os dados. */
       return {
         props: {
           posts: dados,
