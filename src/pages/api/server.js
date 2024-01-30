39a6e50bd1652c0f69fb42f71ee65f8eee867496
victor <victorhugomotabueno@gmail.com>
2024-01-30 09:36:35 -0300

    Update para url do firebase

--- src/pages/api/server.js
+++ src/pages/api/server.js
@@ -1,8 +1,4 @@
 // server.js
-
-const porta = 2112;
-const endereco = "10.20.46.27";
-
-const serverApi = `http://${endereco}:${porta}`;
+const serverApi = `https://petshop-victor-bueno-2024-default-rtdb.firebaseio.com/`;
 
 export default serverApi;
